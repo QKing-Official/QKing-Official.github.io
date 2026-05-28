f93abf7a64538759bb05568587febbd802f0b592
QKing-Official <qkingsoftware@gmail.com>
2026-05-28 18:30:09 +0000

    Initial commit.

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+<div>
+    <div>
+    <h1 style={{ color: 'blue' }}> Welcome! </h1>
+    </div>
+
+    <div>
+      <p> I am a dutch developer who has a fascination for computers and everything around it.
+        I have landed on programming really quickly because of that and I ain't stopping anytime soon!
+      </p>
+    </div>
+
+    <hr></hr>
+
+    <div>
+      <h2> My projects</h2>
+      <p> I have made a lot of projects so far and I am always looking for new challenges to take on!</p>
+    </div>
+</div>
+
+  );
+}

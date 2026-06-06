d7b907a9c9a8b728763cd4ecb3482ed7e696803f
QKing-Official <qkingsoftware@gmail.com>
2026-06-06 08:32:23 +0000

    added new project

--- app/page.tsx
+++ app/page.tsx
@@ -28,8 +28,20 @@ export default function Home() {
         <p> The os a custom app support and has tools required to run them.</p>
         <Link 
           href="https://github.com/QKing-Official/OS" 
-          className="inline-block mt-3 px-6 py-3 bg-blue-600 hover:bg-blue-700 active:scale-95 text-white font-medium rounded-lg text-center no-underline transition"
-        >
+          className="inline-block mt-3 px-6 py-3 bg-blue-600 hover:bg-blue-700 active:scale-95 text-white font-medium rounded-lg text-center no-underline transition">
+          Check it out on GitHub!
+        </Link>
+    </div>
+    <br></br>
+        <div className="bg-slate-950 text-amber-300 p-4 rounded-xl border-2 border-orange-500 shadow-[0_0_20px_rgba(249,115,22,0.6)]">
+        <h2> CCPL </h2>
+        <p> My own little transpiled programming language. </p>
+        <p> This language has bash-like syntax for easy of use..</p>
+        <p> It compiles to C and can be run on any system with a C compiler to ensure powerful programs to be made with ease</p>
+        <p> It comes with it's own package manager called Barite</p>
+        <Link 
+          href="https://github.com/QKing-Official/CCPL" 
+          className="inline-block mt-3 px-6 py-3 bg-blue-600 hover:bg-blue-700 active:scale-95 text-white font-medium rounded-lg text-center no-underline transition">
           Check it out on GitHub!
         </Link>
     </div>
